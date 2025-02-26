410451cf7f905f5644ab68f88b76f09a5204e1a5
James <oconnelljd1@gmail.com>
2025-02-25 22:28:53 -0400

    removed code that ended up in the package

--- Assets/RedHeadToolz/SOExample.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SOExample", menuName = "RedHeadToolz/SOExample")]//, order = 0)]
-public class SOExample : ScriptableObject
-{
-    private string _id;
-    
-    public string ID => _id;
-}
